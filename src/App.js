469d5efef0d05de50be555600e9cbf2f881855c2
ALAABDESSAMED <93202870+ALAABDESSAMED@users.noreply.github.com>
2022-01-26 16:07:19 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React  from "react";
+ 
+ import "./App.css";
+import TodoApp from "./component/TodoApp/TodoApp";
+
+
+function App() {
+  return (
+    <div>
+       <TodoApp />
+    </div>
+  );
+}
+
+export default App;
